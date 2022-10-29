eab79c6870fd1c36b6ea3726165c4d7669415970
zhufucdev <1095291434@qq.com>
2022-10-30 07:17:58 +0800

    Bump version code

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
 
             packageName = "NUISTin"
-            packageVersion = "1.1.0"
+            packageVersion = "1.2.1"
             description = "NUIST auto login"
             copyright = "©2022 zhufucdev. All rights reserved."
             vendor = "zhufucdev"
